065cb86ea9bf329905c3bad6e3f874711f83e978
Paulo Moura <paulociecomp@gmail.com>
2015-02-02 18:04:54 -0300

    organização dos assets

---
+++ vendor/assets/javascripts/vendor.js
@@ -0,0 +1,27 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require library/jquery/jquery-1.11.0.min
+//= require library/jquery/jquery-ui-1.10.4.custom.min
+//= require library/retina/retina.min
+//= require library/fancybox/jquery.fancybox.pack
+//= require library/bootstrap/bootstrap.min
+//= require library/validate/jquery.validate.min
+//= require library/jFlickrFeed/jflickrfeed.min
+//= require library/carouFredSel/jquery.carouFredSel-6.2.1-packed
+//= require library/isotope/isotope.pkgd.min
+//= require map.min
+//= require library/mediaelementjs/mediaelement-and-player.min
+//= require library/slider-revolution/js/jquery.themepunch.tools.min
+//= require library/slider-revolution/js/jquery.themepunch.revolution.min
+//= require slider.min
+//= require theme.min
