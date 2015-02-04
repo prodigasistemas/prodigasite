a8988fc5f36ffed59762703ddfe3e73a259f2cda
Paulo Moura <paulociecomp@gmail.com>
2015-02-04 11:53:20 -0300

    configuracao de assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,9 +4,9 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Rails.root.join("fonts")
+Rails.application.config.assets.paths << Rails.root.join("fonts")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+# Rails.application.config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
