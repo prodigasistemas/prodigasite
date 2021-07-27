6b14ddc60961b0959bf3b40f3b1faeaea6bc5e53
Paulo Igor <piagodinho@gmail.com>
2021-07-27 16:19:39 -0300

    update precompile assets config

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Prodigasite
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
+    #
+
+    config.assets.initialize_on_precompile = false
   end
 end
