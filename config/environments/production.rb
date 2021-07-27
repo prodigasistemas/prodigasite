38192dd18bd8be49f577eca5adaa4e7f1c7a8de2
Paulo Igor <piagodinho@gmail.com>
2021-07-27 16:27:05 -0300

    optimize precompile assets config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,6 @@ Rails.application.configure do
   config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  config.assets.precompile = false
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
